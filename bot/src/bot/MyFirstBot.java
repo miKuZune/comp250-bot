37c40ac4e27daf3fd6e936c2759c3fed3553fc63
mikuzune <maxwfarley@live.co.uk>
2018-04-04 03:18:30 +0100

    Added the beginnings of creating formations and sending them to the enemy base when ready.

--- bot/src/bot/MyFirstBot.java
+++ bot/src/bot/MyFirstBot.java
@@ -33,8 +33,6 @@ public class MyFirstBot extends AbstractionLayerAI
     UnitType rangedType;
     UnitType lightType;
     UnitType heavyType;
-	
-    Formation form = new Formation();
     
     //Stats trackers
     int workersTrained = 0;
@@ -48,7 +46,10 @@ public class MyFirstBot extends AbstractionLayerAI
     int numOfCollecters = 2;
     int minNumOfAttackWorkers = 4;
     
+    int fullFormationSize = 4;
+    
     List<Unit> collecterWorkers = new LinkedList<Unit>();
+    List<Unit> formation = new LinkedList<Unit>();
     
     public MyFirstBot(UnitTypeTable a_utt)
     {
@@ -82,25 +83,7 @@ public class MyFirstBot extends AbstractionLayerAI
 		// TODO Auto-generated method stub
 		PhysicalGameState pgs = gs.getPhysicalGameState();
 		Player p = gs.getPlayer(player);
-		//Base behaviour
-		/*for(Unit u : pgs.getUnits()) 
-		{
-			if(u.getType() == baseType && u.getPlayer() == player && gs.getActionAssignment(u) == null)
-			{baseBehavior(u, p, pgs);}
-		}
-		//Barracks behaviour
-		for(Unit u : pgs.getUnits())
-		{
-			if(u.getType() == barracksType && u.getPlayer() == player && gs.getActionAssignment(u) == null) {barrackBehavior(u, p, pgs);}
-		}
-		//behavior of attack units
-		for(Unit u : pgs.getUnits())
-		{
-			if(u.getType().canAttack && u.getType() == rangedType && !u.getType().canHarvest && u.getPlayer() == player && gs.getActionAssignment(u) == null) {turretUnitBehaviour(u, p, gs);}
-			else if(u.getType().canAttack && !u.getType().canHarvest && u.getPlayer() == player && gs.getActionAssignment(u) == null) {meleeUnitBehavior(u, p, gs);}//meleeUnitBehavior
-		}*/
-		
-		
+
 		for (Unit u : pgs.getUnits())
 		{
 			//Base behaviour
@@ -117,17 +100,29 @@ public class MyFirstBot extends AbstractionLayerAI
 			//Setup collecter workers
 			if(collecterWorkers.size() < numOfCollecters)
 			{
-				if(u.getType() == workerType && u.getPlayer() == p.getID() && collecterWorkers.isEmpty())
+				if(u.getType() == workerType && u.getPlayer() == p.getID() && collecterWorkers.isEmpty()){collecterWorkers.add(u);}
+				else if(u.getType() == workerType && u.getPlayer() == p.getID() && !collecterWorkers.isEmpty())
 				{
-					collecterWorkers.add(u);
-				}else if(u.getType() == workerType && u.getPlayer() == p.getID() && !collecterWorkers.isEmpty())
-				{
+					boolean isAlreadyInList = false;
 					for(int i = 0; i < collecterWorkers.size(); i++)
 					{
 						if(u != collecterWorkers.get(i))
 						{
 							collecterWorkers.add(u);
 						}
+					}
+				}
+			}
+			//Setup formation
+			if(formation.size() < fullFormationSize)
+			{
+				if(u.getType() == rangedType && u.getPlayer() == p.getID() && formation.isEmpty()) {formation.add(u); System.out.println("Added ranged to formation.");}
+				else if(u.getType() == heavyType && u.getPlayer() == p.getID() && !formation.isEmpty())
+				{
+					if(!formation.contains(u))
+					{
+						formation.add(u);
+						System.out.println("Added heavy to formation");
 					}
 				}
 			}
@@ -141,10 +136,10 @@ public class MyFirstBot extends AbstractionLayerAI
 				}
 			}
 		}
+		
 		//Define behaviour for workers that will be collecting resoures.
 		workerBehavior(collecterWorkers,p,pgs);
-		
-
+		FormationBehaviour(formation,p, gs);
 		
 		return translateActions(player,gs);
 	}
@@ -153,12 +148,13 @@ public class MyFirstBot extends AbstractionLayerAI
 	public void baseBehavior(Unit u, Player p, PhysicalGameState pgs)
 	{
 		int nworkers = 0;
-		
+		int nranged = 0;
 		for(Unit u2 : pgs.getUnits())
 		{
 			if(u2.getType() == workerType && u2.getPlayer() == p.getID()) {nworkers++;}
+			if(u2.getType() == rangedType && u2.getPlayer() == p.getID()) {nworkers++;}
 		}
-		if(nworkers < numOfCollecters + minNumOfAttackWorkers && p.getResources() >= workerType.cost) 
+		if(nworkers < numOfCollecters + minNumOfAttackWorkers && p.getResources() >= workerType.cost && nranged < 2) 
 		{
 			train(u, workerType); 
 		}
@@ -166,10 +162,30 @@ public class MyFirstBot extends AbstractionLayerAI
 	
 	public void barrackBehavior(Unit u, Player p, PhysicalGameState pgs)
 	{
+		int rangedCount = 0;
+		int heavyCount = 0;
+		
+		for(Unit u2 : pgs.getUnits())
+		{
+			if(u2.getPlayer() == p.getID() && u2.getType() == rangedType){rangedCount++;}
+			else if (u2.getPlayer() == p.getID() && u2.getType() == heavyType) {heavyCount++;}
+		}
+		
+		if(rangedCount < 2 && p.getResources() >= rangedType.cost)
+		{
+			train(u, rangedType);
+		}else if (rangedCount < 3 && p.getResources() >= rangedType.cost)
+		{
+			train(u, rangedType);
+		}else if(heavyCount < 3 && p.getResources() >= rangedType.cost) 
+		{
+			train(u, heavyType);
+		}
+		
 		//Create a ranged unit who will guard the base
 		
 		//Create a ranged unit as the base of the formation
-		if(form.rangedUnit == null && p.getResources() >= rangedType.cost)
+		/*if(form.rangedUnit == null && p.getResources() >= rangedType.cost)
 		{
 			train(u, rangedType);
 			for(Unit u2 : pgs.getUnits())
@@ -177,7 +193,6 @@ public class MyFirstBot extends AbstractionLayerAI
 				if(u2.getType() == rangedType && u2.getPlayer() == p.getID())
 				{
 					form.rangedUnit = u2;
-					System.out.println(form.rangedUnit);
 				}
 			}
 		}
@@ -200,12 +215,7 @@ public class MyFirstBot extends AbstractionLayerAI
 					}
 				}
 			}			
-		}
-		
-		
-		//if(p.getResources() >= rangedType.cost && rangedTrained == 0) {train(u, rangedType); rangedTrained++;}
-		
-		//else if(p.getResources() >= lightType.cost && rangedTrained > 0) {train(u, lightType); lightTrained++;}
+		}*/
 	}
 	
 	public void meleeUnitBehavior(Unit u, Player p, GameState gs)
@@ -214,11 +224,24 @@ public class MyFirstBot extends AbstractionLayerAI
 		Unit closestEnemy = null;
 		int closestDistance = 0;
 		
+		Unit enemyBarracks = null;
+		Unit enemyStockPile = null;
 		for(Unit u2 : pgs.getUnits())
 		{
+			//Find the enemy barracks
+			if(u2.getType() == baseType && u2.getPlayer() != p.getID())
+			{
+				enemyStockPile = u2;
+			}//Find the enemy barracks
+			if(u2.getType() == barracksType && u2.getPlayer() != p.getID())
+			{
+				enemyBarracks = u2;
+			}
+			
+			//Find the closest enemy
 			if(u2.getPlayer() >= 0 && u2.getPlayer() != p.getID())
 			{
-				int d = Math.abs(u2.getX() - u.getX()) - Math.abs(u2.getY() - u.getY());
+				int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
 				if(closestEnemy == null || d < closestDistance)
 				{
 					closestEnemy = u2;
@@ -226,20 +249,67 @@ public class MyFirstBot extends AbstractionLayerAI
 				}
 			}
 		}
-		if(closestEnemy != null)
+		if(closestDistance < 0) {closestDistance *= -1;}
+		
+		if(closestDistance <= u.getAttackRange())
 		{
-			attack (u, closestEnemy);
+			attack(u, closestEnemy);
+		}
+		else if(enemyStockPile != null)
+		{
+			attack(u, enemyStockPile);
+		}else if (enemyBarracks != null)
+		{
+			attack(u, enemyBarracks);
+		}else if(closestEnemy != null)
+		{
+			attack(u, closestEnemy);
 		}
 	}
 	
 	
-	public void WaitForFullFormation(Unit u, Player p, GameState gs)
+	public void FormationBehaviour(List<Unit> currFormation, Player p, GameState gs)
 	{
-		//Move to base formation position
+		Unit closestEnemy = null;
+		int closestDistance = 0;
 		
-		//Wait until all of formation is there
+		Unit base = null;
+		Unit enemyBase = null;
+		for(Unit u2 : gs.getUnits())
+		{
+			if(u2.getType() == baseType && u2.getPlayer() == p.getID()) {base = u2;}
+			else if(u2.getType() == baseType && u2.getPlayer() != p.getID()) {enemyBase = u2;}
+			
+		}
 		
-		//Begin attack once ready.
+		for(int i = 0; i < currFormation.size(); i++)
+		{
+			if(currFormation.size() < fullFormationSize)
+			{
+				move(currFormation.get(i), base.getX() + 3 - i, base.getY() + 3);
+			}else 
+			{
+				if(enemyBase != null) {attack(currFormation.get(i), enemyBase);}
+				else 
+				{
+					for(Unit u2 : gs.getUnits())
+					{
+						int d = Math.abs(u2.getX() - currFormation.get(i).getX()) + Math.abs(u2.getY() - currFormation.get(i).getY());
+						if(closestEnemy == null || d < closestDistance)
+						{
+							closestEnemy = u2;
+							closestDistance = d;
+						}
+						if(closestEnemy != null)
+						{
+							attack(currFormation.get(i), closestEnemy);
+						}
+					}
+					
+				}
+			}
+			
+		}
 	}
 	
 	//Used ranged units as turrets to set up defensive stuff.
